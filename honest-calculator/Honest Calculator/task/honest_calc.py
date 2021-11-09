1c03310b948559d595d63f386ac8bd5f1073d46f
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-11-09 21:49:00 +0200

    Honest calc, stage 4

--- honest-calculator/Honest Calculator/task/honest_calc.py
+++ honest-calculator/Honest Calculator/task/honest_calc.py
@@ -1,4 +1,31 @@
 # write your code here
+def check(v1, v2, v3):
+    msg_6 = " ... lazy"
+    msg_7 = " ... very lazy"
+    msg_8 = " ... very, very lazy"
+    msg_9 = "You are"
+    msg = ""
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+    if (v1 == 1 or v2 == 1) and (v3 == "*"):
+        msg += msg_7
+    if (v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-"):
+        msg += msg_8
+    if msg != "":
+        msg = msg_9 + msg
+        print(msg)
+
+
+def is_one_digit(v):
+    try:
+        v = int(v)
+    except ValueError:
+        return False
+    if (v > -10) and (v < 10):
+        return True
+    return False
+
+
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
@@ -42,17 +69,19 @@ while continue_calculations:
             if oper not in ["+", "-", "*", "/"]:
                 is_valid_operator = False
                 print(msg_2)
-            elif oper == "+":
-                result = x + y
-            elif oper == "-":
-                result = x - y
-            elif oper == "*":
-                result = x * y
-            elif y == 0:
-                is_valid_divisor = False
-                print(msg_3)
             else:
-                result = x / y
+                check(x, y, oper)
+                if oper == "+":
+                    result = x + y
+                elif oper == "-":
+                    result = x - y
+                elif oper == "*":
+                    result = x * y
+                elif y == 0:
+                    is_valid_divisor = False
+                    print(msg_3)
+                else:
+                    result = x / y
     print(result)
     answer_4 = ""
     while answer_4 not in ["y", "n"]:
