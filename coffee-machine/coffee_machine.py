32ae7a40f6bcefed6016a53ed39b72805cc10400
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-11-14 21:11:11 +0200

    Coffee machine, stage 5

--- coffee-machine/coffee_machine.py
+++ coffee-machine/coffee_machine.py
@@ -14,46 +14,63 @@ def print_state(water, milk, coffee, cups, money):
     print(cups, "of disposable cups")
     print(money, "of money")
 
-def execute_operation(water, milk, coffee, cups, money, action):
-    if not ((action == "fill") or (action == "take")):
-        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-        coffee_type = input()
-        if coffee_type == "1":
-            water -= 250
-            coffee -= 16
-            money += 4
-            cups -= 1
-        elif coffee_type == "2":
-            water -= 350
-            milk -= 75
-            coffee -= 20
-            money += 7
-            cups -= 1
-        elif coffee_type == "3":
-            water -= 200
-            milk -= 100
-            coffee -= 12
-            money += 6
-            cups -= 1
+def check_ingredients(coffee_type):
+    if remaining_cups < 1:
+        return False
+    if coffee_type == "1":
+        if (remaining_water < 250
+                or remaining_coffee < 16):
+            return False
+    if coffee_type == "2":
+        if (remaining_water < 350
+                or remaining_milk < 75
+                or remaining_coffee < 20):
+            return False
+    if coffee_type == "3":
+        if (remaining_water < 200
+                or remaining_milk < 100
+                or remaining_coffee < 12):
+            return False
+    return True
+
+while True:
+    print("Write action (buy, fill, take):")
+    action = input()
+    if action == "exit":
+        break
+    elif action == "remaining":
+        print_state(remaining_water, remaining_milk, remaining_coffee, remaining_cups, remaining_money)
     elif action == "fill":
         print("Write how many ml of water you want to add:")
-        water += int(input())
+        remaining_water += int(input())
         print("Write how many ml of milk you want to add:")
-        milk += int(input())
+        remaining_milk += int(input())
         print("Write how many grams of coffee beans you want to add:")
-        coffee += int(input())
+        remaining_coffee += int(input())
         print("Write how many disposable coffee cups you want to add:")
-        cups += int(input())
+        remaining_cups += int(input())
     elif action == "take":
-        print("I gave you $", money)
-        money = 0
-
-    return water, milk, coffee, cups, money
-
-
-print_state(remaining_water, remaining_milk, remaining_coffee, remaining_cups, remaining_money)
-print("Write action (buy, fill, take):")
-action = input()
-remaining_water, remaining_milk, remaining_coffee, remaining_cups, remaining_money = execute_operation(
-    remaining_water, remaining_milk, remaining_coffee, remaining_cups, remaining_money, action)
-print_state(remaining_water, remaining_milk, remaining_coffee, remaining_cups, remaining_money)
+        print("I gave you $", remaining_money)
+        remaining_money = 0
+    elif action == "buy":
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        coffee_type = input()
+        has_ingredients = check_ingredients(coffee_type)
+        if has_ingredients:
+            if coffee_type == "1":
+                remaining_water -= 250
+                remaining_coffee -= 16
+                remaining_money += 4
+                remaining_cups -= 1
+            elif coffee_type == "2":
+                remaining_water -= 350
+                remaining_milk -= 75
+                remaining_coffee -= 20
+                remaining_money += 7
+                remaining_cups -= 1
+            elif coffee_type == "3":
+                remaining_water -= 200
+                remaining_milk -= 100
+                remaining_coffee -= 12
+                remaining_money += 6
+                remaining_cups -= 1
