2a338866d2efe4a2229f4be4f01620c5d9ef96f0
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-11-24 22:28:21 +0200

    Dominoes, stage 2

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -30,27 +30,42 @@ class Dominoes:
         if (max_computer_index == None) and (max_player_index == None):
             self.status = "computer"
         elif (max_computer_index != None) and (max_player_index == None):
-            self.domino_snake.append(self.computer_dominoes[max_computer_index])
+            self.snake.append(self.computer_dominoes[max_computer_index])
             del self.computer_dominoes[max_computer_index]
             self.status = "player"
         elif (max_computer_index == None) and (max_player_index != None):
-            self.domino_snake.append(self.player_dominoes[max_player_index])
+            self.snake.append(self.player_dominoes[max_player_index])
             del self.player_dominoes[max_player_index]
             self.status = "computer"
         elif self.computer_dominoes[max_computer_index][0] > self.player_dominoes[max_player_index][0]:
-            self.domino_snake.append(self.computer_dominoes[max_computer_index])
+            self.snake.append(self.computer_dominoes[max_computer_index])
             del self.computer_dominoes[max_computer_index]
             self.status = "player"
         else:
-            self.domino_snake.append(self.player_dominoes[max_player_index])
+            self.snake.append(self.player_dominoes[max_player_index])
             del self.player_dominoes[max_player_index]
             self.status = "computer"
+
+    def get_stock_size(self):
+        return len(self.stock)
+
+    def get_computer_dominoes_size(self):
+        return len(self.computer_dominoes)
+
+    def get_snake(self):
+        return self.snake
+
+    def get_player_dominoes(self):
+        return self.player_dominoes
+
+    def get_status(self):
+        return self.status
 
     def __init__(self):
         self.stock = []
         self.player_dominoes = []
         self.computer_dominoes = []
-        self.domino_snake = []
+        self.snake = []
         self.status = ""
 
         self.generate_stock()
@@ -66,10 +81,19 @@ class Dominoes:
 
         self.initialize_snake()
 
-        print("Stock pieces:", self.stock)
-        print("Computer pieces:", self.computer_dominoes)
-        print("Player pieces:", self.player_dominoes)
-        print("Domino snake:", self.domino_snake)
-        print("Status:", self.status)
-
+print("="*70)
 dominoes = Dominoes()
+print("Stock size: {}".format(dominoes.get_stock_size()))
+print("Computer pieces: {}\n".format(dominoes.get_computer_dominoes_size()))
+print(dominoes.get_snake()[0])
+print("\nYour pieces:")
+cnt = 1
+for item in dominoes.get_player_dominoes():
+    print("{}:{}".format(cnt, item))
+    cnt += 1
+
+print()
+if dominoes.get_status() == "computer":
+    print("Status: Computer is about to make a move. Press Enter to continue...")
+else:
+    print("Status: It's your turn to make a move. Enter your command.")
