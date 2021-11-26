d86c61e72606c730d149f2856b75d7610ac7f857
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-11-26 19:25:00 +0200

    Dominoes, stage 3

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -1,6 +1,11 @@
 import random
 
 class Dominoes:
+    # def generate_stock(self):
+    #     for x in range(7):
+    #         for y in range(7):
+    #             self.stock.append([x, y])
+
     def generate_stock(self):
         for x in range(7):
             for y in range(x, 7):
@@ -26,7 +31,13 @@ class Dominoes:
                 else:
                     if self.computer_dominoes[x][0] > self.computer_dominoes[max_computer_index][0]:
                         max_computer_index = x
-
+        # for x in self.computer_dominoes:
+        #     if x[0] == x[1]:
+        #         if max_computer_item == []:
+        #             max_computer_item = x
+        #         else:
+        #             if x[0] > max_computer_item[0]:
+        #                 max_computer_item = x
         if (max_computer_index == None) and (max_player_index == None):
             self.status = "computer"
         elif (max_computer_index != None) and (max_player_index == None):
@@ -61,6 +72,59 @@ class Dominoes:
     def get_status(self):
         return self.status
 
+    def is_game_over(self):
+        # check if any player is out of dominoes
+        if len(self.player_dominoes) == 0 or len(self.computer_dominoes) == 0:
+            return True
+
+        # check if all dominoes with particular value are out and corner items are the same
+        corner_item = self.snake[0][0]
+        cnt = 1
+        for item in self.snake:
+            if item[0] == corner_item:
+                cnt += 1
+            if item[1] == corner_item:
+                cnt += 1
+        if cnt == 8:
+            if corner_item == self.snake[len(self.snake)][1]:
+                return True
+        return False
+
+    def get_winner(self):
+        if len(self.player_dominoes) == 0:
+            return "player"
+        elif len(self.computer_dominoes) == 0:
+            return "computer"
+        else:
+            return "draw"
+
+    def make_computer_turn(self):
+        index = random.randint(len(self.computer_dominoes) * -1, len(self.computer_dominoes))
+        if index < 0:
+            self.snake.insert(0, self.computer_dominoes[abs(index) - 1])
+            del self.computer_dominoes[abs(index) - 1]
+        elif index > 0:
+            self.snake.append(self.computer_dominoes[abs(index) - 1])
+            del self.computer_dominoes[abs(index) - 1]
+        else:
+            index = random.randint(len(self.stock) * -1, len(self.stock))
+            self.computer_dominoes.append(self.stock[index])
+            del self.stock[index]
+        self.status = "player"
+
+    def make_player_turn(self, index):
+        if index < 0:
+            self.snake.insert(0, self.player_dominoes[abs(index) - 1])
+            del self.player_dominoes[abs(index) - 1]
+        elif index > 0:
+            self.snake.append(self.player_dominoes[index - 1])
+            del self.player_dominoes[index - 1]
+        else:
+            index = random.randint(len(self.stock) * -1, len(self.stock))
+            self.player_dominoes.append(self.stock[index])
+            del self.stock[index]
+        self.status = "computer"
+
     def __init__(self):
         self.stock = []
         self.player_dominoes = []
@@ -81,19 +145,71 @@ class Dominoes:
 
         self.initialize_snake()
 
-print("="*70)
-dominoes = Dominoes()
-print("Stock size: {}".format(dominoes.get_stock_size()))
-print("Computer pieces: {}\n".format(dominoes.get_computer_dominoes_size()))
-print(dominoes.get_snake()[0])
-print("\nYour pieces:")
-cnt = 1
-for item in dominoes.get_player_dominoes():
-    print("{}:{}".format(cnt, item))
-    cnt += 1
 
-print()
-if dominoes.get_status() == "computer":
-    print("Status: Computer is about to make a move. Press Enter to continue...")
+def snake_to_string(snake):
+    snake_as_string = ""
+    if len(snake) < 7:
+        for piece in snake:
+            snake_as_string += str(piece)
+    else:
+        snake_as_string += str(snake[0])
+        snake_as_string += str(snake[1])
+        snake_as_string += str(snake[2])
+        snake_as_string += str(snake[-3])
+        snake_as_string += str(snake[-2])
+        snake_as_string += str(snake[-1])
+    return snake_as_string
+
+
+def validate_user_input(user_input, player_dominoes):
+    try:
+        user_input_as_int = int(user_input)
+    except ValueError:
+        return False
+    if abs(user_input_as_int) > len(player_dominoes):
+        return False
+    return True
+
+
+dominoes = Dominoes()
+while True:
+    print("="*70)
+    print("Stock size: {}".format(dominoes.get_stock_size()))
+    print("Computer pieces: {}".format(dominoes.get_computer_dominoes_size()))
+
+    print()
+    print(snake_to_string(dominoes.get_snake()))
+    print()
+
+    print("Your pieces:")
+    cnt = 1
+    for item in dominoes.get_player_dominoes():
+        print("{}:{}".format(cnt, item))
+        cnt += 1
+
+    print()
+    if dominoes.is_game_over():
+        break
+
+    if dominoes.get_status() == "computer":
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+    else:
+        print("Status: It's your turn to make a move. Enter your command.")
+
+    user_input = input()
+    while (dominoes.get_status() == "player") and not validate_user_input(user_input, dominoes.get_player_dominoes()):
+        print("Invalid input. Please try again.")
+        user_input = input()
+
+    if dominoes.get_status() == "computer":
+        dominoes.make_computer_turn()
+    else:
+        dominoes.make_player_turn(int(user_input))
+
+winner = dominoes.get_winner()
+if winner == "player":
+    print("Status: The game is over. You won!")
+elif winner == "computer":
+    print("Status: The game is over. The computer won!")
 else:
-    print("Status: It's your turn to make a move. Enter your command.")
+    print("Status: The game is over. It's a draw!")
