6723b14a5d5687450b3d925a62e75c8e8369b286
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-12-19 22:02:42 +0200

    Knight tour puzzle, stage 5

--- knight-tour-puzzle/knight_tour_puzzle.py
+++ knight-tour-puzzle/knight_tour_puzzle.py
@@ -17,10 +17,10 @@ def calculate_delimiter(some_num):
     return result
 
 
-def get_dashes(delimiter_multiplier, y_delimiter, board_y):
+def get_dashes(delimiter_multiplier, y_delimiter, board_x):
     result = " " * y_delimiter
     result += "--"  # first dash
-    result += "-" * (board_y * (delimiter_multiplier + 1))
+    result += "-" * (board_x * (delimiter_multiplier + 1))
     result += "-"  # last dash
     return result
 
@@ -36,12 +36,12 @@ def get_footer(delimiter_multiplier, y_delimiter, board_y):
     return result
 
 
-def get_possible_moves(x, y, board_x, board_y):
+def get_possible_moves(x, y, board_y, board_x, occupied_spaces):
     dirty_moves_list = [[x + 1, y + 2], [x + 1, y - 2], [x + 2, y + 1], [x + 2, y - 1],
                         [x - 1, y + 2], [x - 1, y - 2], [x - 2, y + 1], [x - 2, y - 1]]
     moves_list = []
     for move in dirty_moves_list:
-        if (move[0] > 0 and move[0] <= board_y and move[1] > 0 and move[1] <= board_x):
+        if (0 < move[0] <= board_x and 0 < move[1] <= board_y and not is_occupied(move[1], move[0], occupied_spaces)):
             moves_list.append(move)
     return moves_list
 
@@ -53,11 +53,11 @@ def space_in_possible_moves(row, column, possible_moves):
     return False
 
 
-def number_of_possible_moves(pos_x, pos_y, board_x, board_y, occupied_spaces):
+def number_of_possible_moves(pos_x, pos_y, board_y, board_x, occupied_spaces):
     cnt = 0
-    for row in range(board_x, 0, -1):
-        for column in range(1, board_y + 1):
-            if space_in_possible_moves(row, column, get_possible_moves(pos_y, pos_x, board_x, board_y)) and not is_occupied(row, column, occupied_spaces):
+    for row in range(board_y, 0, -1):
+        for column in range(1, board_x + 1):
+            if space_in_possible_moves(row, column, get_possible_moves(pos_y, pos_x, board_y, board_x, occupied_spaces)) and not is_occupied(row, column, occupied_spaces):
                 cnt += 1
     return cnt
 
@@ -69,50 +69,90 @@ def is_occupied(pos_x, pos_y, occupied_spaces):
     return False
 
 
+def get_move(board_y, board_x, possible_moves):
+    cnt = 0
+    while True:
+        if cnt == 0:
+            cnt = 1
+            pos = input("Enter your next move:")
+        else:
+            pos = input("Invalid move! Enter your next move:")
+        try:
+            pos_x, pos_y = pos.split(sep=" ")
+            pos_x = int(pos_x)
+            pos_y = int(pos_y)
+            assert pos_y in range(1, board_y + 1)
+            assert pos_x in range(1, board_x + 1)
+            if space_in_possible_moves(pos_y, pos_x, possible_moves):
+                break
+        except Exception:
+            pass
+    return pos_x, pos_y
+
+
 # Initialize board
 while True:
     board = input("Enter your board dimensions: ")
     try:
-        board_y, board_x = board.split(sep=" ")
-        board_x = int(board_x)
+        board_x, board_y = board.split(sep=" ")
         board_y = int(board_y)
-        assert board_x >= 0
-        assert board_y >= 0
+        board_x = int(board_x)
+        assert board_y > 0
+        assert board_x > 0
     except Exception:
         print("Invalid dimensions!")
     else:
         break
 
-# Initialize starting position
+
 while True:
     pos = input("Enter the knight's starting position: ")
     try:
         pos_x, pos_y = pos.split(sep=" ")
         pos_x = int(pos_x)
         pos_y = int(pos_y)
-        assert pos_y in range(1, board_x + 1)
-        assert pos_x in range(1, board_y + 1)
+        assert pos_y in range(1, board_y + 1)
+        assert pos_x in range(1, board_x + 1)
     except Exception:
         print("Invalid dimensions!")
     else:
         break
+# Initialize starting position
 
-del_mult = calculate_delimiter_multiplier(board_x, board_y)
-y_delimiter = calculate_delimiter(board_y)
-possible_moves = get_possible_moves(pos_x, pos_y, board_x, board_y)
+del_mult = calculate_delimiter_multiplier(board_y, board_x)
+y_delimiter = calculate_delimiter(board_x)
 occupied_spaces = [[pos_x, pos_y]]
-# Print the board
-print(get_dashes(del_mult, y_delimiter, board_y))
-for row in range(board_x, 0, -1):
-    to_print = f"{get_row_prefix(row, y_delimiter)}| "
-    for column in range(1, board_y + 1):
-        if (pos_x == column) and (pos_y == row):
-            to_print += " " * (del_mult - 1) + "X "
-        elif space_in_possible_moves(row, column, possible_moves):
-            to_print += " " * (del_mult - 1) + f"{number_of_possible_moves(row, column, board_x, board_y, occupied_spaces)} "
-        else:
-            to_print += ("_" * del_mult) + " "
-    to_print += "|"
-    print(to_print)
-print(get_dashes(del_mult, y_delimiter, board_y))
-print(get_footer(del_mult, y_delimiter, board_y))
+possible_moves = get_possible_moves(pos_x, pos_y, board_y, board_x, occupied_spaces)
+visits = 0
+
+# while len(possible_moves) > 0:
+while True:
+    visits += 1
+    print(get_dashes(del_mult, y_delimiter, board_x))
+    for row in range(board_y, 0, -1):
+        to_print = f"{get_row_prefix(row, y_delimiter)}| "
+        for column in range(1, board_x + 1):
+            if (pos_x == column) and (pos_y == row):
+                to_print += " " * (del_mult - 1) + "X "
+            elif is_occupied(row, column, occupied_spaces):
+                to_print += " " * (del_mult - 1) + "* "
+            elif space_in_possible_moves(row, column, possible_moves):
+                to_print += " " * (del_mult - 1) + f"{number_of_possible_moves(row, column, board_y, board_x, occupied_spaces)} "
+            else:
+                to_print += ("_" * del_mult) + " "
+        to_print += "|"
+        print(to_print)
+    print(get_dashes(del_mult, y_delimiter, board_x))
+    print(get_footer(del_mult, y_delimiter, board_x))
+    print()
+    if len(possible_moves) == 0:
+        break
+    pos_x, pos_y = get_move(board_y, board_x, possible_moves)
+    occupied_spaces.append([pos_x, pos_y])
+    possible_moves = get_possible_moves(pos_x, pos_y, board_y, board_x, occupied_spaces)
+
+if visits == (board_x * board_y):
+    print("What a great tour! Congratulations!")
+else:
+    print("No more possible moves!")
+    print(f"Your knight visited {visits} squares!")
