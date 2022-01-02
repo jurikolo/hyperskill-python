0982dae6ccd17575a4813bae909f31898bd0c9ef
Jurijs Kolomijecs <jurikolo@yandex.com>
2022-01-02 10:44:53 +0200

    Password hacker, stage 3

--- password-hacker/password_hacker.py
+++ password-hacker/password_hacker.py
@@ -2,36 +2,51 @@ import socket
 import argparse
 import itertools
 import string
+import os
+
+
+def get_common_passwords():
+    with open("passwords.txt", "r") as f:
+        contents = f.read()
+    return contents
+
+
+def get_password_modifications(password):
+    list_to_iterate = []
+    result = []
+    for char in password:
+        list_to_iterate.append([char, char.upper()])
+    for x in itertools.product(*list_to_iterate):
+        password = ""
+        for char in x:
+            password += char
+        result.append(password)
+    return result
+
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("hostname")
 parser.add_argument("port")
-
 args = parser.parse_args()
+
 hostname = args.hostname
 port = int(args.port)
-
-dictionary = list(string.ascii_lowercase) + list(string.digits)
+common_passwords = get_common_passwords().split("\n")
 
 password_is_hacked = False
-password_length = 0
 response = ""
-
 
 with socket.socket() as client_socket:
     address = (hostname, port)
     client_socket.connect(address)
 
     while not password_is_hacked or response == "Too many attempts":
-        password_length += 1
-        my_iter = itertools.combinations(dictionary, password_length)
-        while True:
+        try:
+            password_modifications = get_password_modifications(common_passwords.pop())
+        except IndexError as e:
+            break
+        for password in password_modifications:
             try:
-                password = ""
-                password_object = next(my_iter)
-                for char in password_object:
-                    password += char
                 data = password.encode()
                 client_socket.send(data)
                 response = client_socket.recv(1024)
